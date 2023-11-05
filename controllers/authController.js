cd03f9b4567320b474f135cb865a1385b9813fc0
Harsh Dixit <harshdixit573@gmail.com>
2023-11-05 20:28:44 +0530

    refresh token added

--- controllers/authController.js
+++ controllers/authController.js
@@ -2,6 +2,24 @@ const User=require('../models/User')
 const bcrypt=require('bcrypt')
 const jwt=require('jsonwebtoken')
 
+const refreshTokenController=(req,res)=>{
+    const { refreshToken }=req.body
+    if(!refreshToken) return res.status(401).json("Refresh Token required")
+    try {
+        const secret=process.env.refreshKey
+        const decoded=jwt.verify(refreshToken,secret)
+        console.log(decoded);
+        const _id=decoded._id
+        const email=decoded.email
+        const accessToken=generateAccessToken({
+            _id,
+            email
+        })
+        return res.status(200).json({"accessToken":accessToken})
+    } catch (error) {
+        res.status(401).json("Invalid Refresh Token")
+    }
+}
 
 const signUpController = async (req,res)=>{
 
@@ -32,21 +50,34 @@ const logInController= async(req,res)=>{
         if(!user) return res.status(400).json("User not found")
         const match=await bcrypt.compare(password,user.password)
         if(!match) return res.status(400).json("Invalid Password")
-        const token= generateToken({
+        const accessToken= generateAccessToken({
             _id:user._id,
             email:user.email,
         })
-        return res.status(200).json(token)
+        const refreshToken= generateRefreshToken({
+            _id:user._id,
+            email:user.email,
+        })        
+        return res.status(200).json({'accessToken':accessToken,
+                                    'refreshToken':refreshToken})
     }
     catch(err){
         console.log(err);
     }
 }
 
-const generateToken=  (data)=>{
-    const secret=process.env.key
-    const token =  jwt.sign(data,secret,{ expiresIn: '180s' })
+//Internal functions 
+
+const generateRefreshToken=  (data)=>{
+    const secret=process.env.refreshKey
+    const token = jwt.sign(data,secret,{ expiresIn: '1y' })
     return token
 }
 
-module.exports={signUpController,logInController}
+const generateAccessToken=  (data)=>{
+    const secret=process.env.key
+    const token =  jwt.sign(data,secret,{ expiresIn: '15m' })
+    return token
+}
+
+module.exports={signUpController,logInController,refreshTokenController}
--- routers/authRouter.js
+++ routers/authRouter.js
@@ -1,7 +1,7 @@
 const router=require('express').Router()
-const {signUpController,logInController} =require('../controllers/authController')
+const {signUpController,logInController,refreshTokenController} =require('../controllers/authController')
 
 router.post('/signup',signUpController)
 router.post('/login',logInController)
-
+router.post('/refresh',refreshTokenController)
 module.exports=router
